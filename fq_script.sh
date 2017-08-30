9651db6be23c5efc2083e4fbfa6cb4903c300cc4
896660689 <896660689@qq.com>
2017-08-30 23:05:06 +0800

    Update fq_script.sh

--- fq_script.sh
+++ fq_script.sh
@@ -12,16 +12,6 @@ if [ ! -d "/etc/storage/dnsmasq.d" ]; then
 	cp -f /etc/resolv.conf /etc/storage/dnsmasq.d/resolv_bak
 fi
 
-if [ ! -f "/etc/storage/dnsmasq.d/userlist" ]; then
-	echo -e "\e[1;36m 创建自定义翻墙规则 \e[0m\n"
-	cat > "/etc/storage/dnsmasq.d/userlist" <<EOF
-# 国内dns优化
-address=/email.163.com/223.6.6.6
-address=/mail.qq.com/119.29.29.29
-EOF
-fi
-chmod 644 /etc/storage/dnsmasq.d/userlist
-
 if [ -d "/etc/storage/dnsmasq.d" ]; then
 	echo -e "\e[1;33m 创建更新脚本 \e[0m\n"
 	wget --no-check-certificate -t 30 -T 60 https://raw.githubusercontent.com/896660689/os/master/tmp_fq_up -qO /tmp/tmp_fq_up
@@ -51,83 +41,6 @@ if [ ! -d "/etc/storage/dnsmasq.d/conf" ]; then
 	mkdir -p /etc/storage/dnsmasq.d/conf
 	echo "address=/localhost/127.0.0.1" > /etc/storage/dnsmasq.d/conf/hosts_fq.conf && chmod 644 /etc/storage/dnsmasq.d/conf/hosts_fq.conf
 fi
-
-echo -e "\e[1;36m 创建自定义广告黑名单 \e[0m\n"
-if [ ! -f "/etc/storage/dnsmasq.d/blacklist" ]; then
-	cat > "/etc/storage/dnsmasq.d/blacklist" <<EOF
-# 请在下面添加广告黑名单
-# 每行输入要屏蔽广告网址不含http://符号
-active.admore.com.cn
-g.163.com
-mtty-cdn.mtty.com
-static-alias-1.360buyimg.com
-image.yzmg.com
-files.jb51.net/image
-common.jb51.net/images
-du.ebioweb.com/ebiotrade/web_images
-www.37cs.com
-fuimg.com
-sinaimg.cn
-ciimg.com
-pic.iidvd.com
-cnzz.com
-statis.api.3g.youku.com
-ad.m.qunar.com
-lives.l.aiseet.atianqi.com
-# 运营商ip劫持
-120.197.89.239
-211.139.178.49
-221.179.46.190
-221.179.46.193
-221.179.46.194
-60.19.29.21
-60.19.29.24
-61.174.50.168
-# 广告IP劫持
-47.89.59.182
-106.75.65.90
-114.55.123.44
-122.225.103.120
-123.59.78.229
-139.224.26.92
-222.186.61.97
-222.187.226.96
-23.235.156.167
-101.201.29.182
-EOF
-fi
-chmod 644 /etc/storage/dnsmasq.d/blacklist
-
-echo -e "\e[1;36m 创建自定义广告白名单 \e[0m\n"
-if [ ! -f "/etc/storage/dnsmasq.d/whitelist" ]; then
-	cat > "/etc/storage/dnsmasq.d/whitelist" <<EOF
-# 请将误杀的网址添加到在下面白名单
-# 每行输入相应准确的网址或关键词即可:
-m.baidu.com
-github.com
-raw.githubusercontent.com
-my.k2
-tv.sohu.com
-toutiao.com
-jd.com
-tejia.taobao.com
-temai.taobao.com
-ai.m.taobao.com
-ai.taobao.com
-re.taobao.com
-shi.taobao.com
-s.click.taobao.com
-s.click.tmall.com
-ju.taobao.com
-dl.360safe.com
-down.360safe.com
-fd.shouji.360.cn
-zhushou.360.cn
-shouji.360.cn
-hot.m.shouji.360tpcdn.com
-EOF
-fi
-chmod 644 /etc/storage/dnsmasq.d/whitelist
 
 if [ -f "/etc/storage/cron/crontabs/$username" ]; then
 	echo -e "\e[1;33m 添加定时计划更新任务 \e[0m\n"
