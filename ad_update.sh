d46d27bfcc5719ad89ea6842a951d5b32bb80dd5
896660689 <896660689@qq.com>
2017-08-31 01:29:35 +0800

    Update ad_update.sh

--- ad_update.sh
+++ ad_update.sh
@@ -4,6 +4,7 @@ LOGTIME=$(date "+%m-%d %H:%M:%S")
 
 echo " 检测AD安装脚本版本 "
 if [ ! -f /tmp/ad_script_up.sh ]; then
+	logger -t "【$LOGTIME】" " 开始运行去广告更新任务..."
 	wget --no-check-certificate https://raw.githubusercontent.com/896660689/os/master/ad_script.sh -O /tmp/ad_script_up.sh;chmod 775 /tmp/ad_script_up.sh
 	cat /tmp/ad_script_up.sh /etc/storage/dnsmasq.d/ad_script.sh | awk '{ print$0}' | sort | uniq -u > /tmp/ad_script_up.txt && sleep 2
 	if [ ! -s "/tmp/ad_script_up.txt" ]; then
@@ -17,7 +18,6 @@ if [ ! -f /tmp/ad_script_up.sh ]; then
 	fi
 fi
 if [ -x "/etc/storage/dnsmasq.d/ad_update.sh" ]; then
-	logger -t "【$LOGTIME】" " 开始运行去广告更新任务..."
 	[ -f /tmp/tmp_ad_up ] && rm -f /tmp/tmp_ad_up
 	# 准备翻墙 AD 文件
 	wget --no-check-certificate -t 10 -T 30 https://raw.githubusercontent.com/896660689/os/master/tmp_ad_up -qO \
