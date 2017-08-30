24b1a43ab196287d89758f5f7a9aab4f30c3f784
896660689 <896660689@qq.com>
2017-08-31 02:18:33 +0800

    Update fq_update.sh

--- fq_update.sh
+++ fq_update.sh
@@ -4,7 +4,6 @@ LOGTIME=$(date "+%m-%d %H:%M:%S")
 
 echo " 检测FQ安装脚本版本 "
 if [ ! -f /tmp/fq_script_up.sh ]; then
-	logger -t "【$LOGTIME】" " 开始运行翻墙更新任务..."
 	wget --no-check-certificate https://raw.githubusercontent.com/896660689/os/master/fq_script.sh -O /tmp/fq_script_up.sh;chmod 775 /tmp/fq_script_up.sh
 	cat /tmp/fq_script_up.sh /etc/storage/dnsmasq.d/fq_script.sh | awk '{ print$0}' | sort | uniq -u > /tmp/fq_script_up.txt && sleep 2
 	if [ ! -s "/tmp/fq_script_up.txt" ]; then
@@ -18,6 +17,7 @@ if [ ! -f /tmp/fq_script_up.sh ]; then
 	fi
 fi
 if [ -x "/etc/storage/dnsmasq.d/fq_update.sh" ]; then
+	logger -t "【$LOGTIME】" " 开始运行翻墙更新任务..."
 	[ -f /tmp/tmp_fq_up ] && rm -f /tmp/tmp_fq_up
 	# 准备翻墙 FQ 文件
 	wget --no-check-certificate -t 10 -T 30 https://raw.githubusercontent.com/896660689/os/master/tmp_fq_up -qO \
